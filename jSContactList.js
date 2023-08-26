f6eed93e8d957ec4cd49a49e0906ce9763cd6585
anyersonjcs <anyersonj@gmail.com>
2023-08-26 12:00:23 -0400

    Anexando Bonus - review-1

--- jSContactList.js
+++ jSContactList.js
@@ -46,6 +46,29 @@ agregarContacto(5, "David", "Diaz", 4163344556, [
   { ciudad: "Cojedes", dirección: "Naguanagua" }
 ]);
 
+// Review-1: Función para actualizar un contacto existente en la lista buscando su id
+function actualizarContacto(id, nombre, apellido, teléfono, ubicaciones) {
+  const contactoExistente = listaContactos.find(contacto => contacto.id === id);
+
+  // Acá creo una condición, de modo que si el contacto existe, actualice sus datos con los nuevos valores
+  if (contactoExistente) {
+    contactoExistente.nombre = nombre;
+    contactoExistente.apellido = apellido;
+    contactoExistente.teléfono = teléfono;
+    contactoExistente.ubicaciones = ubicaciones;
+
+    console.log("Contacto actualizado correctamente");
+    console.log(JSON.stringify(listaContactos, null, 2));
+  } else {
+    // Pero si el contacto no existe, se mostrará un mensaje de error
+    console.log("Lo siento. No se encontró ningún contacto con ese id");
+  }
+}
+
+actualizarContacto(1, "Anyerson Jose", "Castellanos", 4121481324111, [
+  { ciudad: "Whasington DC", dirección: "The White House" }
+]);
+
 // Función para borrar un contacto existente de la lista, borrando también los nuevos parámetros solicitados
 function borrarContactoXId(id) {
   listaContactos = listaContactos.filter(contacto => contacto.id !== id);
